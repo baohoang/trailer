6f0addbf79201a627986b1febeef89e83b9332f8
thuctapvtn2014 <hoangquocbao151091@gmail.com>
2014-06-29 14:36:42 +0700

    edit file aaa.java

--- Trailer/src/aaa.java
+++ Trailer/src/aaa.java
@@ -1,4 +1,21 @@
 
 public class aaa {
-
+	private int a,b;
+	public aaa(){
+		this.a=0;
+		this.b=0;
+	}
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	public int getB() {
+		return b;
+	}
+	public void setB(int b) {
+		this.b = b;
+	}
+	
 }
